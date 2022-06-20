3965e87e6a58c156f4bbd0a1447f99901c49a539
Ruan J. Gräbe <ruan.grabe@gmail.com>
2022-06-20 14:26:18 +0200

    add link volumes event handler - FIXED

--- src/main/java/org/matsim/analysis/LinkVolumesEventHandler.java
+++ src/main/java/org/matsim/analysis/LinkVolumesEventHandler.java
@@ -10,45 +10,25 @@ import org.matsim.vehicles.Vehicle;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LinkVolumesEventHandler implements LinkLeaveEventHandler {
 
-    private static final Logger log = Logger.getLogger(LinkVolumesEventHandler.class);
     Id<Link> watchedLink = Id.createLinkId("908198570004f"); // link just before entrance
-    final Map<String,Double> vehicleLeaveLinkEvent = new HashMap<>();
+    public final Map<String,Double> vehicleLeaveLinkEvent = new HashMap<>();
 
     @Override
     public void handleEvent(LinkLeaveEvent linkLeaveEvent) {
 
-        final double leaveTime = linkLeaveEvent.getTime();
+        double leaveTime = linkLeaveEvent.getTime() / 3600.;
         final Id<Vehicle> vehicleId = linkLeaveEvent.getVehicleId();
         watchedLink = linkLeaveEvent.getLinkId();
 
-        if (linkLeaveEvent.getLinkId() == watchedLink) {
+        if (linkLeaveEvent.getLinkId().equals(watchedLink)) {
             vehicleLeaveLinkEvent.put( vehicleId.toString(), leaveTime );
-        }
-
-        {
-            String path = "scenarios/serengeti-park-v1.0/output/linkVolumes.csv";
-            File file = new File(path);
-            BufferedWriter bw1 = new BufferedWriter(new FileWriter(file)); // todo
-
-            bw1.write("VehicleId;Time [s]");
-            bw1.newLine();
-
-            for (Map.Entry<String, Double> entry : vehicleLeaveLinkEvent.entrySet()) {
-                String key = entry.getKey();
-                Object value = entry.getValue();
-                bw1.write(key + ";" + value );
-
-            }
-            bw1.newLine();
-
-            bw1.close();
-            log.info("Done");
-            log.info("Link volumes written to " + path);
+//            vehicleLeaveLinkEvent.merge(String.valueOf(leaveTime), 1, Integer::sum ); // to create bins, but use Map<String,Integer>
         }
 
     }
--- src/main/java/org/matsim/analysis/SimpleAnalysis.java
+++ src/main/java/org/matsim/analysis/SimpleAnalysis.java
@@ -1,16 +1,17 @@
 package org.matsim.analysis;
 
-import org.matsim.api.core.v01.Id;
-import org.matsim.api.core.v01.network.Link;
-import org.matsim.core.api.experimental.events.EventsManager;
+import org.apache.log4j.Logger;
 import org.matsim.core.events.EventsUtils;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 
 public class SimpleAnalysis {
+
+    private static final Logger log = Logger.getLogger(LinkVolumesEventHandler.class);
 
     public static void main(String[] args) {
 
@@ -20,11 +21,46 @@ public class SimpleAnalysis {
 
         EventsUtils.readEvents(manager, "scenarios/serengeti-park-v1.0/output/output-serengeti-park-v1.0-run1/serengeti-park-v1.0-run1.output_events.xml.gz");
 
-        // todo: finish link event handler
         // code at: https://github.com/Janekdererste/matsim-serengeti-park-hodenhagen/blob/3f8b2dbdaf92c08746a25c6dbad8686954d0e493/src/main/java/org/matsim/analyze/LinkEventHandler.java
         var linkHandler = new LinkVolumesEventHandler();
         manager.addHandler(linkHandler);
         EventsUtils.readEvents(manager, "scenarios/serengeti-park-v1.0/output/output-serengeti-park-v1.0-run1/serengeti-park-v1.0-run1.output_events.xml.gz");
 
+        var linkHashmap = linkHandler.vehicleLeaveLinkEvent;
+        writeLinkVolumes(linkHashmap);
+    }
+
+    static void writeLinkVolumes(Map<String, Double> linkMap) {
+        String path = "scenarios/serengeti-park-v1.0/output/linkVolumes.csv";
+        File file = new File(path);
+        BufferedWriter bw1;
+        try {
+            bw1 = new BufferedWriter(new FileWriter(file));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            bw1.write("VehicleId;Time [h]");
+            bw1.newLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (Map.Entry<String, Double> entry : linkMap.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            try {
+                bw1.write(key + ";" + value );
+                bw1.newLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        try {
+            bw1.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        log.info("Done");
+        log.info("Link volumes written to 'scenarios/serengeti-park-v1.0/output/linkVolumes.csv'");
     }
 }
